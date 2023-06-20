0c2acb9cad8e03f2422251c70f5252a261d14bb6
Andrew Vinci <andrewvinci92@gmail.com>
2023-06-20 12:02:37 -0400

    Chapter 1 and Part of 2 Done

---
+++ app/src/main/java/com/example/geoquiz_summer/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.geoquiz_summer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.geoquiz_summer.Question
+import com.example.geoquiz_summer.R
+import com.example.geoquiz_summer.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    //private lateinit var trueButton: Button
+    //private lateinit var falseButton: Button
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //trueButton = findViewById(R.id.true_button)
+        //falseButton = findViewById(R.id.false_button)
+
+        binding.trueButton.setOnClickListener { view: View ->
+
+            Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
+            /*
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+            */
+        }
+
+        binding.falseButton.setOnClickListener { view : View ->
+
+            Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_SHORT).show()
+
+            /*
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+             */
+        }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            binding.questionText.setText(questionTextResId)
+        }
+
+    }
+}
