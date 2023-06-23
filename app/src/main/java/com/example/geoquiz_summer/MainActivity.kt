6f92d6b698f3008adcaf4d6e8167fba03b5465ab
Andrew Vinci <andrewvinci92@gmail.com>
2023-06-23 15:12:25 -0400

    Added ViewModel and SavedStateInstance to take care of Lifecycle Issues

--- app/src/main/java/com/example/geoquiz_summer/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz_summer/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.example.geoquiz_summer.Question
 import com.example.geoquiz_summer.R
 import com.example.geoquiz_summer.databinding.ActivityMainBinding
@@ -18,39 +19,33 @@ class MainActivity : AppCompatActivity() {
     //private lateinit var falseButton: Button
 
     private lateinit var binding: ActivityMainBinding
+    private val quizViewModel: QuizViewModel by viewModels()
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-
-    private var currentIndex = 0
-    private var correctAnswers = 0
+    //private var correctAnswers = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "oncreate(Bundle?) called")
+        Log.d(TAG, "onCreate(Bundle?) called")
+
+        // Alternative to View Binding
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+
+        // Alternative to View Binding
         //trueButton = findViewById(R.id.true_button)
         //falseButton = findViewById(R.id.false_button)
 
         binding.trueButton.setOnClickListener { view: View ->
 
             checkAnswer(true)
-
             binding.trueButton.isEnabled = false
             binding.falseButton.isEnabled = false
 
             //Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
-
             /*
             Toast.makeText(
                 this,
@@ -63,12 +58,10 @@ class MainActivity : AppCompatActivity() {
         binding.falseButton.setOnClickListener { view : View ->
 
             checkAnswer(false)
-
             binding.falseButton.isEnabled = false
             binding.trueButton.isEnabled = false
 
             //Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_SHORT).show()
-
             /*
             Toast.makeText(
                 this,
@@ -77,26 +70,33 @@ class MainActivity : AppCompatActivity() {
             ).show()             */
         }
 
+        // Updates the question for the first question
         updateQuestion()
 
+        // Binding on click listener for question Text View
         binding.questionText.setOnClickListener{
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.currentIndex = (quizViewModel.currentIndex + 1) % quizViewModel.questionBankSize
             updateQuestion()
         }
 
+        // Binding on click listener for next button
         binding.nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            //currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
 
             binding.trueButton.isEnabled = true
             binding.falseButton.isEnabled = true
         }
 
+        // Binding on click listener for previous button
         binding.prevButton.setOnClickListener{
-            if(currentIndex == 0){
-                currentIndex = questionBank.size
+            if(quizViewModel.currentIndex == 0){
+                quizViewModel.currentIndex = quizViewModel.questionBankSize
             }
-            currentIndex = (currentIndex - 1) % questionBank.size
+            quizViewModel.currentIndex = (quizViewModel.currentIndex - 1) % quizViewModel.questionBankSize
+            quizViewModel.moveBack()
+
             updateQuestion()
 
             binding.trueButton.isEnabled = true
@@ -105,6 +105,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // Overrides of various lifecycle states
     override fun onStart(){
         super.onStart()
         Log.d(TAG, "onStart() Called")
@@ -130,16 +131,20 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onDestroy Called")
     }
 
+    // Updates the question based on the current index
     private fun updateQuestion(){
-        val questionTextResId = questionBank[currentIndex].textResId
+        //val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         binding.questionText.setText(questionTextResId)
     }
 
+    // Checks the answer to see if its correct and matches model data
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        //val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
 
         val messageResId = if (userAnswer  == correctAnswer){
-            correctAnswers += 1
+            quizViewModel.correctAnswers += 1
             R.string.correct_toast
         }
         else{
@@ -148,9 +153,9 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
 
-        if(currentIndex == 5){
-            Toast.makeText(this, (correctAnswers.toDouble() / questionBank.size.toDouble()).toString() + "%", Toast.LENGTH_SHORT).show()
-            correctAnswers = 0
+        if(quizViewModel.currentIndex == 5){
+            Toast.makeText(this, (quizViewModel.correctAnswers.toDouble() / quizViewModel.questionBankSize.toDouble()).toString() + "%", Toast.LENGTH_SHORT).show()
+            quizViewModel.correctAnswers = 0
         }
     }
 }
