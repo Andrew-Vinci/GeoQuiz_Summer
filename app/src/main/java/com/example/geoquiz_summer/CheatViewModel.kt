da55dba37e9c74d8b1ca720cd36922a8e81864cd
Andrew Vinci <andrewvinci92@gmail.com>
2023-07-03 10:22:16 -0400

    Added all features of Geoquiz besides 1

--- app/src/main/java/com/example/geoquiz_summer/CheatViewModel.kt
+++ app/src/main/java/com/example/geoquiz_summer/CheatViewModel.kt
@@ -15,18 +15,4 @@ class CheatViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel
 
      var answerWasClicked = false
 
-     /*
-     var answerText: Int
-          get() = savedStateHandle.get(ANSWER_TEXT) ?: 0
-          set(value) = savedStateHandle.set(ANSWER_TEXT, value)
-
-     var isAnswerTrue: Boolean
-          get() = savedStateHandle.get(IS_ANSWER_TRUE) ?: false
-          set(value) = savedStateHandle.set(IS_ANSWER_TRUE, value)
-
-     private var answerIsTrue: Boolean
-          get() = savedStateHandle.get(ANSWER_IS_TRUE) ?: false
-          set(value) = savedStateHandle.set(ANSWER_IS_TRUE, value)
-      */
-
 }
